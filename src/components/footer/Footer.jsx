1bde505b77fc24a79e948cdbf66f1e590cac403f
RuslanGon <ruslan441006@gmail.com>
2025-01-28 10:51:04 +0200

    wer

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -1,12 +1,14 @@
 import styles from './page.module.css'
 import Image from 'next/image.js';
-
+import Link from 'next/link';
 
 const Footer = () => {
   return (
     <footer className={styles.container}>
       <div>2023 MyApp. All rights reserved</div>
-      <div></div>
+      <div className={styles.social}>
+        <Link href="https://www.instagram.com/?locale=ru"><Image className={styles.icon} src='/vk.png' width={15} height={15}/></Link>
+      </div>
     </footer>
   );
 };
