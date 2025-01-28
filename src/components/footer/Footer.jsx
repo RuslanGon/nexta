9f0582b9f656068254f87f5e59e3cf9f4943e7a9
RuslanGon <ruslan441006@gmail.com>
2025-01-28 11:24:02 +0200

    wer

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -7,7 +7,33 @@ const Footer = () => {
     <footer className={styles.container}>
       <div>2023 MyApp. All rights reserved</div>
       <div className={styles.social}>
-        <Link href="https://www.instagram.com/?locale=ru"><Image className={styles.icon} src='/vk.png' width={15} height={15}/></Link>
+        <Link href="https://vk.com/">
+          <Image
+            className={styles.icon}
+            alt="VK logo"
+            src="/vk.png"
+            width={15}
+            height={15}
+          />
+        </Link>
+        <Link href="https://www.instagram.com/?locale=ru">
+          <Image
+            className={styles.icon}
+            alt="insta logo"
+            src="/inst.png"
+            width={15}
+            height={15}
+          />
+        </Link>
+        <Link href="https://x.com/?lang=ru">
+          <Image
+            className={styles.icon}
+            alt="twitter logo"
+            src="/twitter.png"
+            width={15}
+            height={15}
+          />
+        </Link>
       </div>
     </footer>
   );
