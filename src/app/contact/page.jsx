b3c2c5f3bb0927cd0e5c8f196a0e488df23ed1ed
RuslanGon <ruslan441006@gmail.com>
2025-01-28 13:06:27 +0200

    wer

--- src/app/contact/page.jsx
+++ src/app/contact/page.jsx
@@ -1,9 +1,37 @@
-import styles from './page.module.css'
+import Button from "@/components/Button/Button.jsx";
+import styles from "./page.module.css";
+import Image from "next/image";
+import Contimg from '../../../public/contimg.jpeg'
+
+
 
 const Contact = () => {
   return (
-    <div>Contact</div>
-  )
-}
+    <div className={styles.container}>
+      <h1 className={styles.title}>Lets Keep in Touch</h1>
+      <div className={styles.content}>
+        <div className={styles.imgContainer}>
+          <Image
+            src={Contimg}
+            alt="contacts"
+            fill={true}
+            className={styles.image}
+          />
+        </div>
+        <form className={styles.form}>
+          <input type="text" placeholder="name" className={styles.input} />
+          <input type="text" placeholder="email" className={styles.input} />
+          <textarea
+            className={styles.textArea}
+            placeholder="message"
+            cols="30"
+            rows="10"
+          ></textarea>
+          <Button url="#" text="Send" />
+        </form>
+      </div>
+    </div>
+  );
+};
 
-export default Contact
+export default Contact;
