8f616f093809e523636088db54c1e4df84a5651e
RuslanGon <ruslan441006@gmail.com>
2025-01-28 12:14:23 +0200

    wer

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,10 +1,22 @@
 import Image from "next/image";
 import styles from "./page.module.css";
+import Button from "@/components/Button/Button.jsx";
+import Hero from '../../public/hero.jpg'
 
 export default function Home() {
   return (
-   <div>
-    Home
-   </div>
+    <main className={styles.container}>
+      <div className={styles.item}>
+        <h1 className={styles.title}>The Future of AI in the next few years </h1>
+        <p className={styles.desc}>
+          Turning your Idea into Reality. We bring together the teams from the
+          global tech industry.
+        </p>
+        <Button url="/portfolio" text="See Our Works" />
+      </div>
+      <div className={styles.iten}>
+        <Image className={styles.img} src={Hero} alt="Hero"/>
+      </div>
+    </main>
   );
 }
