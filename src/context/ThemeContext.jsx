38668c453734d99b36f292e4b14ea37b79cc8a48
RuslanGon <ruslan441006@gmail.com>
2025-01-29 17:30:34 +0200

    wer

--- src/context/ThemeContext.jsx
+++ src/context/ThemeContext.jsx
@@ -13,3 +13,36 @@ export const ThemeProvider = ({ children }) => {
     </ThemeContext.Provider>
   );
 };
+
+// "use client";
+// import { createContext, useState, useEffect } from "react";
+
+// export const ThemeContext = createContext();
+
+// export const ThemeProvider = ({ children }) => {
+//   // Состояние для хранения текущей темы
+//   const [mode, setMode] = useState("dark");
+
+//   // Загружаем сохранённую тему из localStorage при монтировании
+//   useEffect(() => {
+//     const savedTheme = localStorage.getItem("theme");
+//     if (savedTheme) {
+//       setMode(savedTheme);
+//     }
+//   }, []);
+
+//   // Функция переключения темы
+//   const toggle = () => {
+//     setMode((prevMode) => {
+//       const newMode = prevMode === "dark" ? "light" : "dark";
+//       localStorage.setItem("theme", newMode); // Сохраняем тему в localStorage
+//       return newMode;
+//     });
+//   };
+
+//   return (
+//     <ThemeContext.Provider value={{ toggle, mode }}>
+//       <div className={`theme ${mode}`}>{children}</div>
+//     </ThemeContext.Provider>
+//   );
+// };
