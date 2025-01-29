ca29e2cccc9376a6ae7000fd3065c8ffae596dfa
RuslanGon <ruslan441006@gmail.com>
2025-01-29 16:42:20 +0200

    wer

--- src/app/blog/[id]/page.jsx
+++ src/app/blog/[id]/page.jsx
@@ -1,9 +1,33 @@
+import Image from "next/image.js";
+import styles from "./page.module.css";
 
-
-const page = () => {
+const BlogId = () => {
   return (
-    <div>page</div>
-  )
-}
+    <div className={styles.container}>
+      <div className={styles.top}>
+        <div className={styles.info}>
+          <h1 className={styles.title}>title</h1>
+          <p className={styles.desc}>desc</p>
+          <div className={styles.author}>
+            <Image
+              src=""
+              alt=""
+              width={40}
+              height={40}
+              className={styles.avatar}
+            />
+            <span className={styles.username}>username</span>
+          </div>
+        </div>
+        <div className={styles.imageContainer}>
+          <Image src="" alt="" fill={true} className={styles.image} />
+        </div>
+      </div>
+      <div className={styles.content}>
+        <p className={styles.text}></p>
+      </div>
+    </div>
+  );
+};
 
-export default page
+export default BlogId;
