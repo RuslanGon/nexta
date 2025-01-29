5d1b6924bfe768d9c656eddfbcb5d6a7989e2bec
RuslanGon <ruslan441006@gmail.com>
2025-01-29 16:33:20 +0200

    wer

--- src/app/blog/page.jsx
+++ src/app/blog/page.jsx
@@ -1,9 +1,21 @@
-import styles from './page.module.css'
+import Link from "next/link";
+import styles from "./page.module.css";
+import Image from "next/image";
 
 const Blog = () => {
   return (
-    <div>Blog</div>
-  )
-}
+    <div className={styles.mainContainer}>
+      <Link href="blog/test" className={styles.container} key="1">
+        <div className={styles.imageContainer}>
+          <Image className={styles.image} width={400} height={250} alt="logo"/>
+        </div>
+        <div className={styles.content}>
+            <h1 className={styles.title}>title</h1>
+            <p className={styles.desc}>desc</p>
+          </div>
+      </Link>
+    </div>
+  );
+};
 
-export default Blog
+export default Blog;
--- src/app/portfolio/[category]/page.jsx
+++ src/app/portfolio/[category]/page.jsx
@@ -1,9 +1,28 @@
-import styles from './page.module.css'
+import Image from "next/image.js";
+import styles from "./page.module.css";
+import Button from "@/components/Button/Button.jsx";
 
-const page = ({params}) => {
+const Category = ({ params }) => {
+  // console.log(params);
+  // const data = getData(params.category)
   return (
-    <div>pagscsdce</div>
-  )
-}
+    <div className={styles.container}>
+      <h1 className={styles.catTitle}>{params.category}</h1>
+      <div className={styles.item}>
+        <h1 className={styles.title}>test</h1>
+        <p className={styles.desc}>desc</p>
+        <Button text="See More" url="#" />
+      </div>
+      <div className={styles.imgContainer}>
+          <Image
+            className={styles.img}
+            fill={true}
+            // src={}
+            //  alt={}
+          />
+        </div>
+    </div>
+  );
+};
 
-export default page
+export default Category;
