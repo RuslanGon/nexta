223f169410f260be19472a478a5bac582e000413
RuslanGon <ruslan441006@gmail.com>
2025-01-30 16:09:56 +0200

    wer

--- src/app/portfolio/[category]/page.jsx
+++ src/app/portfolio/[category]/page.jsx
@@ -1,39 +1,49 @@
-import Image from "next/image.js";
-import styles from "./page.module.css";
-import Button from "@/components/Button/Button.jsx";
-import items from './data.js'
-import { notFound } from "next/navigation.js";
+import Image from 'next/image'
+import styles from './page.module.css'
+import Button from '@/components/Button/Button'
+import { items } from './data'
+import { notFound } from 'next/navigation';
+import Port from '../../../../public/port.jpg'
 
-const getData = (cat) => {
-const data = items[cat]
+const getData = (cat) =>{
+  const data = items[cat]
 
-if (data){
-  return data
+  if (data){
+    return data
+  }
+  return notFound()
 }
-return notFound()
-}
 
-const Category = ({ params }) => {
+
+const Category = ({params}) => {
   // console.log(params);
   const data = getData(params.category)
   return (
     <div className={styles.container}>
       <h1 className={styles.catTitle}>{params.category}</h1>
-      <div className={styles.item}>
-        <h1 className={styles.title}>test</h1>
-        <p className={styles.desc}>desc</p>
-        <Button text="See More" url="#" />
-      </div>
-      <div className={styles.imgContainer}>
+
+      {data.map((item)=>(
+      <div className={styles.item} key={item.id}>
+        <div className={styles.content}>
+          <h1 className={styles.title}>{item.title}</h1>
+          <p className={styles.desc}>{item.desc}</p>
+          <Button text="See More" url="#" />
+        </div>
+        <div className={styles.imgContainer}>
           <Image
             className={styles.img}
             fill={true}
-            // src={}
-            //  alt={}
+            src={Port}
+             alt={item.title}
           />
         </div>
-    </div>
-  );
-};
+      </div>
 
-export default Category;
+      ))}
+
+
+    </div>
+  )
+}
+
+export default Category
