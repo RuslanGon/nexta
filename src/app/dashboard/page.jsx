ef4106288200176df51dbb6427275b14a4da63c1
RuslanGon <ruslan441006@gmail.com>
2025-01-30 15:32:32 +0200

    wer

--- src/app/dashboard/page.jsx
+++ src/app/dashboard/page.jsx
@@ -1,6 +1,31 @@
+'use client'
+import { useEffect, useState } from 'react'
 import styles from './page.module.css'
 
 const Dashboard = () => {
+const [data, setData] = useState([]);
+  const [error, setError] = useState(false);
+  const [isLoading, setIsLoading] = useState(true);
+
+  useEffect(() => {
+    const getData = async () => {
+      const res = await fetch("https://jsonplaceholder.typicode.com/posts", {
+        cache: "no-store",
+      });
+
+      if (!res.ok) {
+        setError(true);
+      }
+      const data = await res.json()
+
+      setData(data)
+      setIsLoading(false)
+    };
+    getData()
+  },[]);
+
+  console.log(data);
+
   return (
     <div>Dashboard</div>
   )
