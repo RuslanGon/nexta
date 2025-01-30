ed1313b4aaf300e4c2b09393f59e33e067ad2436
RuslanGon <ruslan441006@gmail.com>
2025-01-30 14:54:29 +0200

    wer

--- src/app/blog/page.jsx
+++ src/app/blog/page.jsx
@@ -1,19 +1,41 @@
 import Link from "next/link";
 import styles from "./page.module.css";
 import Image from "next/image";
+import Fetch from '../../../public/fetch.jpg'
 
-const Blog = () => {
+async function getData() {
+  const res = await fetch("https://jsonplaceholder.typicode.com/posts", {
+    cache: "no-store",
+  });
+
+  if (!res.ok) {
+    throw new Error("Failed to fetch data");
+  }
+
+  return res.json();
+}
+
+const Blog = async () => {
+  const data = await getData();
   return (
     <div className={styles.mainContainer}>
-      <Link href="blog/test" className={styles.container} key="1">
-        <div className={styles.imageContainer}>
-          <Image className={styles.image} width={400} height={250} alt="logo"/>
-        </div>
-        <div className={styles.content}>
-            <h1 className={styles.title}>title</h1>
-            <p className={styles.desc}>desc</p>
+      {data.map((item) => (
+        <Link href={`blog/${item.id}`}className={styles.container} key={item.id}>
+          <div className={styles.imageContainer}>
+            <Image
+            src={Fetch}
+              className={styles.image}
+              width={400}
+              height={250}
+              alt="logo"
+            />
           </div>
-      </Link>
+          <div className={styles.content}>
+            <h1 className={styles.title}>{item.title}</h1>
+            <p className={styles.desc}>{item.body}</p>
+          </div>
+        </Link>
+      ))}
     </div>
   );
 };
