ea36aa34362c8be6338e9830a407319975ce7891
RuslanGon <ruslan441006@gmail.com>
2025-01-31 11:45:32 +0200

    цук

--- src/app/api/posts/route.js
+++ src/app/api/posts/route.js
@@ -4,13 +4,12 @@ import { NextResponse } from "next/server";
 
 export const GET = async () => {
   try {
-    await connect(); // Подключение к базе данных
-    const posts = await Post.find(); // Получение всех постов
+    await connect(); 
+    const posts = await Post.find(); 
 
-    return NextResponse.json(posts, { status: 200 }); // Возвращаем JSON-ответ
+    return NextResponse.json(posts, { status: 200 }); 
 
   } catch (error) {
-    console.error("Database connection error:", error);
     return new NextResponse("Error in connection DB", { status: 500 });
   }
 };
