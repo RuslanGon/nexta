58409dd803f7927c164701254c8e83388f7063df
RuslanGon <ruslan441006@gmail.com>
2025-01-31 12:24:33 +0200

    wer

--- src/app/blog/[id]/page.jsx
+++ src/app/blog/[id]/page.jsx
@@ -1,14 +1,9 @@
-import Image from "next/image.js";
+import Image from "next/image";
 import styles from "./page.module.css";
 import Fetch from '../../../../public/fetch.jpg'
 
 async function getData(id) {
-  // const res = await fetch(`https://jsonplaceholder.typicode.com/posts/${id}`, 
-  const res = await fetch(`http://localhost:3000/api/posts/${id}`, 
-
-  {
-    // cache: "no-store",
-  });
+  const res = await fetch(`http://localhost:3000/api/posts?id=${id}`);
 
   if (!res.ok) {
     throw new Error("Failed to fetch data");
@@ -19,13 +14,14 @@ async function getData(id) {
 
 const BlogId = async ({ params }) => {
   const data = await getData(params.id);
+
   return (
     <div className={styles.container}>
       <div className={styles.top}>
         <div className={styles.info}>
           <h1 className={styles.title}>{data.title}</h1>
           <p className={styles.desc}>{data.content}</p>
-          <p className={styles.name}>{item.username}</p>
+          <p className={styles.name}>{data.username}</p>
           <div className={styles.author}>
             <Image
               src={Fetch}
@@ -34,15 +30,12 @@ const BlogId = async ({ params }) => {
               height={40}
               className={styles.avatar}
             />
-            <span className={styles.username}>username</span>
+            <span className={styles.username}>{data.username}</span>
           </div>
         </div>
         <div className={styles.imageContainer}>
-          <Image src={Fetch} alt="logo" fill={true} className={styles.image} />
+          <Image src={Fetch} alt="logo" fill className={styles.image} />
         </div>
-      </div>
-      <div className={styles.content}>
-        <p className={styles.text}></p>
       </div>
     </div>
   );
