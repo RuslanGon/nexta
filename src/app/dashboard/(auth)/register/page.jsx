77fd51cc91a0f9b41cffcd32c805294db91f217d
RuslanGon <ruslan441006@gmail.com>
2025-02-02 12:39:44 +0200

    wrr

--- src/app/dashboard/(auth)/register/page.jsx
+++ src/app/dashboard/(auth)/register/page.jsx
@@ -1,9 +1,42 @@
-import Link from 'next/link.js'
-import styles from './page.module.css'
+"use client";
+import React, { useState } from "react";
+import styles from "./page.module.css";
+import Link from "next/link";
+import { useRouter } from "next/navigation";
 
 const Register = () => {
+  const [error, setError] = useState(null);
+
+  const router = useRouter();
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    const name = e.target[0].value;
+    const email = e.target[1].value;
+    const password = e.target[2].value;
+
+    try {
+      const res = await fetch("/api/auth/register", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({
+          name,
+          email,
+          password,
+        }),
+      });
+      res.status === 201 &&
+        router.push("/dashboard/login?success=Account has been created");
+    } catch (err) {
+      setError(err);
+      console.log(err);
+    }
+  };
+
   return (
-<div className={styles.container}>
+    <div className={styles.container}>
       <h1 className={styles.title}>Create an Account</h1>
       <h2 className={styles.subtitle}>Please sign up to see the dashboard.</h2>
       <form onSubmit={handleSubmit} className={styles.form}>
@@ -33,7 +66,7 @@ const Register = () => {
         Login with an existing account
       </Link>
     </div>
-  )
-}
+  );
+};
 
-export default Register
+export default Register;
